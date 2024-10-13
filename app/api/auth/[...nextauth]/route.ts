2ba641ddae97a17dad6f97830f61e646443c394a
woodbeary <lopez731@gmail.com>
2024-10-12 17:42:26 -0700

    oops wrong name for env key

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,15 @@
 import NextAuth from "next-auth"
 import TwitterProvider from "next-auth/providers/twitter"
 
-if (!process.env.TWITTER_CLIENT_ID || !process.env.TWITTER_CLIENT_SECRET) {
-  throw new Error('Missing Twitter OAuth credentials');
+if (!process.env.X_OAUTH_CLIENT_ID || !process.env.X_OAUTH_CLIENT_SECRET) {
+  throw new Error('Missing X OAuth credentials');
 }
 
 const handler = NextAuth({
   providers: [
     TwitterProvider({
-      clientId: process.env.TWITTER_CLIENT_ID,
-      clientSecret: process.env.TWITTER_CLIENT_SECRET,
+      clientId: process.env.X_OAUTH_CLIENT_ID,
+      clientSecret: process.env.X_OAUTH_CLIENT_SECRET,
     }),
   ],
   // Add any additional configuration here
