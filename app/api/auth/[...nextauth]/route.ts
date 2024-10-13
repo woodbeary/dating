f3b7118da9e72d06a8986e864e2f054c43189014
woodbeary <lopez731@gmail.com>
2024-10-12 17:39:57 -0700

    env varaible keys

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth"
 import TwitterProvider from "next-auth/providers/twitter"
 
+if (!process.env.TWITTER_CLIENT_ID || !process.env.TWITTER_CLIENT_SECRET) {
+  throw new Error('Missing Twitter OAuth credentials');
+}
+
 const handler = NextAuth({
   providers: [
     TwitterProvider({
